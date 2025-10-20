6c9fd72bf90654a38409b7a7e9892b300d6568ee
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-10-20 14:22:39 -0500

    Add Localization

---
+++ Assets/Scripts/Json/CustomizeJson.cs
@@ -0,0 +1,10 @@
+public class CustomizeJson
+{
+    private int bodiesIndex;
+    private int bodypartsIndex;
+    private int eyesIndex;
+    private int glovesIndex;
+    private int headpartsIndex;
+    private int mouthandNosesIndex;
+    private int tailsIndex;
+}
---
+++ Assets/Scripts/Menu/CustomizeManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CustomizeManager : MonoBehaviour
+{
+    [Header("Body Parts")]
+    [SerializeField] private Transform bodies;
+    [SerializeField] private Transform bodyparts;
+    [SerializeField] private Transform eyes;
+    [SerializeField] private Transform gloves;
+    [SerializeField] private Transform headparts;
+    [SerializeField] private Transform mouthandNoses;
+    [SerializeField] private Transform tails;
+
+
+}
