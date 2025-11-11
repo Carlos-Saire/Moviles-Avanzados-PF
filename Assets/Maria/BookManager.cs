81a254ace7d553f8e884ccaea506957564b8db75
maradw <maria.mendoza@tecsup.edu.pe>
2025-11-11 15:52:24 -0500

    books minigame

---
+++ Assets/Maria/BookManager.cs
@@ -0,0 +1,89 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookManager : MonoBehaviour
+{
+    int objToCreate = 4;
+    [SerializeField] Button[] booksInLibrary;
+    [SerializeField] GameObject openPanelBook;
+    [SerializeField] GameObject noteInBook;
+    [SerializeField] GameObject noteParent;
+    [SerializeField] Button closeBook;
+    private bool[] hasNote;
+
+    private GameObject[] noteInstances;
+    private int currentBookIndex = -1;
+
+    void Start()
+    {
+        openPanelBook.SetActive(false);
+        hasNote = new bool[booksInLibrary.Length];
+       
+        noteInstances = new GameObject[booksInLibrary.Length];
+        GetRandomNote();
+        for (int i =  0;  i < booksInLibrary.Length; i++)
+        {
+            int k = i;
+           booksInLibrary[i].onClick.AddListener(() => SetActive(k));
+        }
+        closeBook.onClick.AddListener(() => CloseBook());
+    }
+
+    void Update()
+    {
+        
+    }
+    void CloseBook()
+    {
+        if (currentBookIndex != -1 && noteInstances[currentBookIndex] != null)
+        {
+            noteInstances[currentBookIndex].SetActive(false);
+        }
+        currentBookIndex = -1;
+        openPanelBook?.SetActive(false);
+       
+    }
+    void SetActive(int index)
+    {
+        if (currentBookIndex != -1 && noteInstances[currentBookIndex] != null)
+            noteInstances[currentBookIndex].SetActive(false);
+
+        openPanelBook.SetActive(true);
+        currentBookIndex = index;
+        if (hasNote[index])
+        {
+
+            if (noteInstances[index] == null)
+            {
+
+                noteInstances[index] = Instantiate(noteInBook, noteParent.transform);
+                noteInstances[index].SetActive(true);
+                Debug.Log(index + " tiene una nota (creada).");
+            }
+            else
+            {
+                noteInstances[index].SetActive(true);
+                Debug.Log(index + " tiene una nota (reutilizada).");
+            }
+        }
+        else
+        {
+            Debug.Log(index + " está vacío.");
+        }
+    }    
+    void GetRandomNote()
+    {
+        int assigned = 0;
+        while (assigned < objToCreate)
+        {
+            int randomIndex = Random.Range(0, booksInLibrary.Length);
+            if (!hasNote[randomIndex])
+            {
+                hasNote[randomIndex] = true;
+                assigned++;
+            }
+        }
+          
+    }
+}
