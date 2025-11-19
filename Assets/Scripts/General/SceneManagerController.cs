b48960b566ce52987aabbdca15b1840b01b13259
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-11-18 22:08:09 -0500

    Add Split Cameras

--- Assets/Scripts/General/SceneManagerController.cs
+++ Assets/Scripts/General/SceneManagerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Unity.Netcode;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -15,23 +16,16 @@ public class SceneManagerController : MonoBehaviour
     {
         SceneManager.LoadScene(scene);
     }
-    public void LoadSceneAsync(string scene)
-    {
-        StartCoroutine(LoadSceneCoroutine(scene));
-    }
-    private IEnumerator LoadSceneCoroutine(string scene)
-    {
-        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(scene);
-
-        while (!asyncOp.isDone)
-        {
-            float progress = asyncOp.progress / 0.9f;
-            OnSceneLoadProgress?.Invoke(progress);
-            yield return null; 
-        }
-    }
     public void LoadScene(string scene, LoadSceneMode mode)
     {
         NetworkManager.Singleton.SceneManager.LoadScene(scene,mode);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;   
+#else
+    Application.Quit();                    
+#endif
     }
 }
--- Assets/Scripts/SinglePlayer/Player/JoinController.cs
+++ Assets/Scripts/SinglePlayer/Player/JoinController.cs
@@ -4,29 +4,28 @@ using UnityEngine.InputSystem;
 
 public class JoinController : MonoBehaviour
 {
-    public PlayerInputManager inputManager;
-
-    [Header("Prefabs de Jugadores")]
     public GameObject playerPrefab;
 
     private List<PlayerInput> jugadores = new List<PlayerInput>();
 
-    void Awake()
-    {
-        inputManager.enabled = false; 
-    }
     private void Start()
     {
-        
+        int cantidad = PlayerPrefs.GetInt("JugadoresSeleccionados", 1);
+
+        for (int i = 0; i < cantidad; ++i)
+        {
+            CrearJugador();
+        }
     }
+
     public void CrearJugador()
     {
         PlayerInput jugador = PlayerInput.Instantiate(
             playerPrefab,
-            jugadores.Count,         
+            jugadores.Count,
             null,
             -1,
-            Keyboard.current          
+            Keyboard.current
         );
 
         jugadores.Add(jugador);
@@ -43,28 +42,35 @@ public class JoinController : MonoBehaviour
 
             if (cam == null)
             {
-                Debug.LogWarning("El prefab no contiene cámara. Agrégale una al jugador.");
+                Debug.LogError("Jugador " + i + " no tiene cámara en el prefab.");
                 continue;
             }
+
+            cam.depth = i; 
 
             switch (cantidad)
             {
                 case 1:
-                    cam.rect = new Rect(0, 0, 1, 1);
+                    cam.rect = new Rect(0f, 0f, 1f, 1f);
                     break;
 
                 case 2:
                     cam.rect = (i == 0)
-                        ? new Rect(0, 0.5f, 1, 0.5f)   
-                        : new Rect(0, 0, 1, 0.5f);     
+                        ? new Rect(0f, 0f, 0.5f, 1f)    
+                        : new Rect(0.5f, 0f, 0.5f, 1f); 
                     break;
 
                 case 3:
+                    if (i == 0) cam.rect = new Rect(0f, 0f, 0.5f, 1f);  
+                    if (i == 1) cam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f); 
+                    if (i == 2) cam.rect = new Rect(0.5f, 0f, 0.5f, 0.5f); 
+                    break;
+
                 case 4:
-                    if (i == 0) cam.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+                    if (i == 0) cam.rect = new Rect(0f, 0.5f, 0.5f, 0.5f);
                     if (i == 1) cam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
-                    if (i == 2) cam.rect = new Rect(0, 0, 0.5f, 0.5f);
-                    if (i == 3) cam.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                    if (i == 2) cam.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+                    if (i == 3) cam.rect = new Rect(0.5f, 0f, 0.5f, 0.5f);
                     break;
             }
         }
