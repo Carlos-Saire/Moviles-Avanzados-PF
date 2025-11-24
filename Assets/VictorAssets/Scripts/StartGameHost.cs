605cb581b1e96d3add1f20d447bf271e99965903
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-11-24 15:02:05 -0500

    StartGameHostCondition

--- Assets/VictorAssets/Scripts/StartGameHost.cs
+++ Assets/VictorAssets/Scripts/StartGameHost.cs
@@ -9,6 +9,7 @@ public class StartGameHost : MonoBehaviour
     [SerializeField] private Button startGameButton;
 
     private List<ICommand> commands;
+
     private void Start()
     {
         startGameButton.gameObject.SetActive(false);
@@ -17,16 +18,47 @@ public class StartGameHost : MonoBehaviour
         {
             startGameButton.gameObject.SetActive(true);
             startGameButton.onClick.AddListener(StartGame);
+
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+
+            UpdateButtonState();
         }
+    }
+
+    private void OnDestroy()
+    {
+        if (NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnected;
+        }
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        UpdateButtonState();
+    }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        UpdateButtonState();
+    }
+
+    private void UpdateButtonState()
+    {
+        bool canStart = NetworkManager.Singleton.ConnectedClients.Count > 1;
+        startGameButton.interactable = canStart;
     }
 
     private void StartGame()
     {
-        for(int i = 0; i < commands.Count; ++i)
+        for (int i = 0; i < commands.Count; ++i)
         {
             CommandQueue.Instance.AddCommand(commands[i]);
         }
     }
+
     public void Configure(List<ICommand> list)
     {
         commands = list;
