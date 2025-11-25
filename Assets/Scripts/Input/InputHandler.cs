9f0c27a02833f4d143f17fc14b028aea2ea5a1ba
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-11-25 05:19:19 -0500

    Finish Mbile

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -18,21 +18,78 @@ public class InputHandler : MonoBehaviour
 #if UNITY_ANDROID|| UNITY_IOS
     private float _width;
     private float _currentPrees;
+
+    private bool lookTouchActive = false;
+    private int lookFingerId = -1;
+    private Vector2 previousPos;
+
+
     private void Start()
     {
         _width = Screen.width / 2;
         UImobile.gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (Touchscreen.current == null) return;
+
+        // Leemos TODOS los dedos
+        var touches = Touchscreen.current.touches;
+
+        foreach (var t in touches)
+        {
+            if (!t.press.isPressed) continue;
+
+            int fingerId = t.touchId.ReadValue();
+            Vector2 pos = t.position.ReadValue();
+
+            if (t.press.wasPressedThisFrame)
+            {
+                if (pos.x >= _width)
+                {
+                    lookTouchActive = true;
+                    lookFingerId = fingerId;
+                    previousPos = pos;
+                    Debug.Log("LOOK ACTIVADO dedo " + fingerId);
+                }
+            }
+
+            if (lookTouchActive && fingerId == lookFingerId)
+            {
+                Vector2 delta = pos - previousPos;
+                previousPos = pos;
+
+                OnLook?.Invoke(delta);
+                Debug.Log("DELTA: " + delta);
+            }
+        }
+
+        foreach (var t in touches)
+        {
+            if (t.press.wasReleasedThisFrame)
+            {
+                int fingerId = t.touchId.ReadValue();
+
+                if (lookTouchActive && fingerId == lookFingerId)
+                {
+                    lookTouchActive = false;
+                    lookFingerId = -1;
+
+                    OnLook?.Invoke(Vector2.zero);
+                    Debug.Log("LOOK DESACTIVADO");
+                }
+            }
+        }
     }
 #endif
     public void InputMove(InputAction.CallbackContext context)
     {
         OnMove?.Invoke(context.ReadValue<Vector2>());
         OnMoveSinglePLayer?.Invoke(context.ReadValue<Vector2>());
-        Debug.Log(context.ReadValue<Vector2>());
+        Debug.Log("Move :" + context.ReadValue<Vector2>());
     }
     public void InputLook(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
 #if UNITY_ANDROID
         if (_width < _currentPrees)
         {
