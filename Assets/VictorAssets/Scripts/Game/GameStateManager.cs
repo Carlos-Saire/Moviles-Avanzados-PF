8df60c1f05a0100ad46a304013fac7441e12c7e3
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-11-30 12:40:31 -0500

    Merge

---
+++ Assets/VictorAssets/Scripts/Game/GameStateManager.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public enum GameState
+{
+    Lobby,
+    Game
+}
+
+public class GameStateManager : NetworkBehaviour
+{
+    public static GameStateManager Instance;
+
+    public NetworkVariable<GameState> CurrentState = new NetworkVariable<GameState>(GameState.Lobby);
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetState(GameState newState)
+    {
+        if (!IsServer) return;
+        CurrentState.Value = newState;
+        Debug.Log("GAME STATE → " + newState);
+    }
+}
